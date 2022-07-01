16e3a4f3aef919883b6e8f57f0f0d6d15c633734
Isha <ishajanorkar4691@gmail.com>
2022-07-01 15:35:50 +0530

    first commit

---
+++ precache-manifest.00fee00c22aa477815ecfbe44b68bd28.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f385403c58b953cb376b",
+    "url": "/static/css/main.9701381d.chunk.css"
+  },
+  {
+    "revision": "f385403c58b953cb376b",
+    "url": "/static/js/main.f385403c.chunk.js"
+  },
+  {
+    "revision": "9882606888b40676ecd7",
+    "url": "/static/js/1.98826068.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc39660f3236ff980f14299518bc2220",
+    "url": "/static/media/award.dc39660f.png"
+  },
+  {
+    "revision": "52423f17570ea8bc97453df553b354b6",
+    "url": "/static/media/phone.52423f17.png"
+  },
+  {
+    "revision": "775dfe7f630b37076c29d0e16e21ffcf",
+    "url": "/static/media/email.775dfe7f.png"
+  },
+  {
+    "revision": "a23093fbfc09d373ba3e73bfee764a7b",
+    "url": "/static/media/address.a23093fb.png"
+  },
+  {
+    "revision": "0ea4931cfb81570c4641efdd71a851e7",
+    "url": "/static/media/me.0ea4931c.png"
+  },
+  {
+    "revision": "bcb3306a08b8a1c94207d7b0c2113acf",
+    "url": "/static/media/sun.bcb3306a.png"
+  },
+  {
+    "revision": "9fd3b8d8cb74a883429dd015e313c1ee",
+    "url": "/static/media/moon.9fd3b8d8.png"
+  },
+  {
+    "revision": "70f40c5a2ff16736306b7c502c3fb190",
+    "url": "/index.html"
+  }
+];
